363679f6ff908996d1d435c091d052e86f10d722
matucs <mehdi.byt@gmail.com>
2020-01-03 18:33:12 +0330

    improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnChanges, SimpleChange } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnChanges{
-  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
-    console.log('changes: ',changes)
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.el.nativeElement.focus();
   }
+  @ViewChild('input', null) el: ElementRef;
 
   title = 'todo';
-  list = [{ id: 0, text: 'hhh', icon: 'bx:bx-radio-circle' }];
+  list = [];
   text = '';
 
   onCheck(index) {
-    this.list[index].icon == 'bx:bx-radio-circle' ? this.list[index].icon = 'fe:check-circle-o':
-    this.list[index].icon = 'bx:bx-radio-circle'
-    console.log(this.list[index].icon)
+    var icon = this.list[index].icon == 'bx:bx-radio-circle' ? 'fe:check-circle-o' : 'bx:bx-radio-circle'
+    var temp = { id: index, text: this.list[index].text, icon: icon, checked: !this.list[index].checked }
+    this.list.splice(index, 1, temp);
   }
   onItemAdd() {
-    this.text != '' ? this.list.push({ id: this.list.length, text: this.text, icon: 'bx:bx-radio-circle' }) : '';
+    this.text != '' ? this.list.push({ id: this.list.length, text: this.text, icon: 'bx:bx-radio-circle', checked: false }) : '';
     this.text = '';
+    this.el.nativeElement.focus();
   }
   onDelete(index) {
     this.list.splice(index, 1)
+  }
+  onRemoveCheckedItems() {
+    var temp = this.list.filter( s => s.checked == false ) 
+    this.list = temp;
+    this.el.nativeElement.focus();
   }
 }
